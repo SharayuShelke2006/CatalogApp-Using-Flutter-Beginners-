82c1abfcc0437ebe010c49a82d2636ab6cab84f0
SharayuShelke2006 <sharayushelke2006@gmail.com>
2026-04-29 00:21:18 +0530

    day 1

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.dark,
+    );
+  }
+}
